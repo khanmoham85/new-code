bf3c951ecbe50c0563ddc57fdf44c1c95b1970b9
khanmoham85 <khanmoham85@gmail.com>
2018-12-08 12:08:57 -0500

    first commit

---
+++ src/test/java/practice/practice/Test.java
@@ -0,0 +1,10 @@
+package practice.practice;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello from test");
+		System.out.println("I made changes from test");
+	}
+
+}
---
+++ src/test/java/practice/practice/Test2.java
@@ -0,0 +1,10 @@
+package practice.practice;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("hello from test");
+		System.out.println("I made changes from test");
+	}
+
+}
